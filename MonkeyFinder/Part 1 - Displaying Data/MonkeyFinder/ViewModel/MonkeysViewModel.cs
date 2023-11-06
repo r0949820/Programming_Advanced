c4c54e9193f1bed796d705f7002a5ec9cee96bd8
r0949820 <r0949820@student.thomasmore.be>
2023-11-06 15:28:40 +0100

    MVVM en navigation

--- MonkeyFinder/Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,63 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null)
+        {
+            return;
+        }
+
+        await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
+            new Dictionary<string, object>
+            {
+                {"Monkey", monkey }
+            });
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+        {
+            return;
+        }
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (monkeys.Count != 0)
+            {
+                Monkeys.Clear();
+
+                foreach (var monkey in monkeys)
+                {
+                    Monkeys.Add(monkey);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
